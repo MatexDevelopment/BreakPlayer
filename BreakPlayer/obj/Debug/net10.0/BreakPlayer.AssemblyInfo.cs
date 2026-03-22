e9e2d6c3cee256b8cf016fe2f3aa0495bb78a2fa
Matex <28.matimateusz@gmail.com>
2026-03-22 21:57:43 +0100

    Add new songs catalog

--- BreakPlayer/obj/Debug/net10.0/BreakPlayer.AssemblyInfo.cs
+++ BreakPlayer/obj/Debug/net10.0/BreakPlayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BreakPlayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfc0bcf5624bfeb919d74a5ef75bea00df39f30e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BreakPlayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BreakPlayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
